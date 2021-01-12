d03c8606fa1abbe77408b65131ec5d89ac004b83
Maarten Mulders <m.th.mulders@xs4all.nl>
2021-01-12 22:32:01 +0100

    Update instructions for cross-compiling

--- cross-compile-armv7.sh
+++ cross-compile-armv7.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
+# echo "deb [arch=armhf] http://ports.ubuntu.com/ubuntu-ports focal main" > /etc/apt/sources.list.d/ubuntu-ports.list
+# echo "deb [arch=armhf] http://ports.ubuntu.com/ubuntu-ports focal-security main" > /etc/apt/sources.list.d/ubuntu-ports.list
+# echo "deb [arch=armhf] http://ports.ubuntu.com/ubuntu-ports focal-updates main" > /etc/apt/sources.list.d/ubuntu-ports.list
+# sudo aptitude update
+# sudo aptitude install pkg-config-arm-linux-gnueabihf libssl-dev:armhf libudev-dev:armhf
+
 PATH=$ARMHF_TOOLCHAIN_PATH/bin:$PATH \
+	PKG_CONFIG=/usr/bin/arm-linux-gnueabihf-pkg-config \
 	cargo build --target armv7-unknown-linux-gnueabihf --release --features ""
