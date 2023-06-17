fd67e050bb9462d07ff1b53583d23f0d1c86d72e
Maarten Mulders <m.th.mulders@xs4all.nl>
2023-06-17 18:59:11 +0200

    Cleanup

--- cross-compile-armv7.sh
+++ cross-compile-armv7.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# echo "deb [arch=armhf] http://ports.ubuntu.com/ubuntu-ports focal main" > /etc/apt/sources.list.d/ubuntu-ports.list
-# echo "deb [arch=armhf] http://ports.ubuntu.com/ubuntu-ports focal-security main" > /etc/apt/sources.list.d/ubuntu-ports.list
-# echo "deb [arch=armhf] http://ports.ubuntu.com/ubuntu-ports focal-updates main" > /etc/apt/sources.list.d/ubuntu-ports.list
-# sudo aptitude update
-# sudo aptitude install pkg-config-arm-linux-gnueabihf libssl-dev:armhf libudev-dev:armhf
-
 rustup target add armv7-unknown-linux-gnueabihf
 
 ls -hl $ARMHF_TOOLCHAIN_PATH/bin
