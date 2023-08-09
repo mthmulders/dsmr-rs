51c5b889eb9ba31eccf6b311930d5e30acc5b2e5
Maarten Mulders <m.th.mulders@xs4all.nl>
2023-08-09 22:13:04 +0200

    Try specifying different environment variables

--- cross-compile-arm.sh
+++ cross-compile-arm.sh
@@ -5,9 +5,9 @@ rustup target add arm-unknown-linux-musleabihf
 
 ls -hl $ARMHF_TOOLCHAIN_PATH/bin
 
-export CC_arm_unknown_linux_gnu=arm-none-linux-gnueabihf-gcc
-export CXX_arm_unknown_linux_gnu=arm-none-linux-gnueabihf-g++
-export AR_arm_unknown_linux_gnu=arm-none-linux-gnueabihf-ar
+export CC_arm_linux_muscleabihf=arm-none-linux-gnueabihf-gcc
+export CXX_arm_linux_muscleabihf=arm-none-linux-gnueabihf-g++
+export AR_arm_linux_muscleabihf=arm-none-linux-gnueabihf-ar
 export CARGO_TARGET_ARM_UNKNOWN_LINUX_MUSCLEABIHF_LINKER=arm-none-linux-gnueabihf-gcc
 
 export PATH=$ARMHF_TOOLCHAIN_PATH/bin:$PATH
